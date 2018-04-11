6b7ad3e114cbf11531a0b0d7edc3a44295c1e211
bjsmith3185 <bjsmith3185@hotmail.com>
2018-04-11 17:45:31 -0400

    this version is working

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -7,56 +7,56 @@ var submitButton = document.getElementById('submit');
 
 var myQuestions = [
     {
-        question: "What burger would you get at Mcdonalds?",
+        question: "1. What burger would you get at Mcdonalds?",
         
         answers: {
-            a: "Big Mac",
-            b: "Whopper",
-            c: "Dave's Big n Juicy",
+            a: "Big Mac,",
+            b: "Whopper,",
+            c: "Dave's Big n Juicy,",
             d: "Jack's Classic"
         },
         correctAnswer: "a"
     },
 
     {
-        question: "What restaurant has the Big Bo Box?",
+        question: "2. What restaurant has the Big Bo Box?",
         answers: {
-            a: "McDonalds",
-            b: "KFC",
-            c: "Zaxby's",
-            d: "Bojangles",
+            a: "McDonalds,",
+            b: "KFC,",
+            c: "Zaxby's,",
+            d: "Bojangles,",
         },
         correctAnswer: "d" 
     },
 
     {
-        question: "Who has the $2.99 Ultimate Jack's Breakfast Platter:",
+        question: "3. Who has the $2.99 Ultimate Jack's Breakfast Platter:",
         answers: {
-            a: "Taco Bell",
-            b: "Hwy 55",
-            c: "Jack n the Box",
+            a: "Taco Bell,",
+            b: "Hwy 55,",
+            c: "Jack n the Box,",
             d: "Red Robbin",
         },
         correctAnswer: "c" 
     },
 
     {
-        question: "Who incorporated Doritos into one of their menu items?",
+        question: "4. Who incorporated Doritos into one of their menu items?",
         answers: {
-            a: "Wendy's",
-            b: "Taco Bell",
-            c: "Mcdonalds",
+            a: "Wendy's,",
+            b: "Taco Bell,",
+            c: "Mcdonalds,",
             d: "Burger King",
         },
         correctAnswer: "b" 
     },
  
     {
-        question: "Who has the best customer service?",
+        question: "5. Who has the best customer service?",
         answers: {
-            a: "Zaxby's",
-            b: "Chick Fila",
-            c: "Jack n the Box",
+            a: "Zaxby's,",
+            b: "Chick Fila,",
+            c: "Jack n the Box,",
             d: "Burger King",
         },
         correctAnswer: "b" 
@@ -68,11 +68,6 @@ var questions;
 var quizContainer;
 var resultsContainer;
 
-setTimeout(timerEnd, 1000 * 5);
-
-function timerEnd() {
-       showResults(questions, quizContainer, resultsContainer)
-    }
 
 
 
@@ -80,7 +75,20 @@ generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
 
 function generateQuiz(questions, quizContainer, resultsContainer, submitButton) {
 
-    function showQuestions(questions, quizContainer) {
+    var count = 15;
+    var counter = setInterval(timer, 1000);
+
+    function timer() {
+        count=count-1;
+        $("#timer").text(count + " Seconds Remaining!");
+        if (count <= 0) {
+            showResults(questions, quizContainer, resultsContainer)
+            clearInterval(counter);
+            return;
+        }
+    }
+
+      function showQuestions(questions, quizContainer) {
         var output = [];
         var answers;
 
@@ -91,7 +99,7 @@ function generateQuiz(questions, quizContainer, resultsContainer, submitButton) 
                 answers.push(
                     '<label>'
                         + '<input type="radio" name="question'+i+'" value="'+letter+'">'
-                        + letter + ':'
+                     //   + letter + ':'
                         + questions[i].answers[letter]
                     +'</label>'
                 );
@@ -100,33 +108,36 @@ function generateQuiz(questions, quizContainer, resultsContainer, submitButton) 
             output.push(
                 '<div class="question">' + questions[i].question + '</div>'
                 + '<div class="answers">' + answers.join('') + '</div>'
-
-                
-            );
+             );
         }
             quizContainer.innerHTML = output.join('');
-        
-
     }
+
+
 
     function showResults(questions, quizContainer, resultsContainer) {
 
-        var answerContainers = quizContainer.querySelector('.answers');
+
+        
+
+
+        var answerContainers = quizContainer.querySelectorAll('.answers');
 
         var userAnswer = '';
         var numCorrect = 0;
-
-        for (var i = 0; i < questions.lenght; i++) {
-            userAnswer = (answerContainers[i].querySelector('input[name=question' +i+']:checked')||{}).value;
-
+        console.log("This is userAnswer: "+ userAnswer);
+        for (var i = 0; i < questions.length; i++) {
+            userAnswer = (answerContainers[i].querySelector('input[name="question' +i+'"]:checked')||{}).value;
+          
             if(userAnswer===questions[i].correctAnswer) {
                 numCorrect++;
+                console.log(numCorrect);      
             }
             
         }
 
             resultsContainer.innerHTML = numCorrect + ' out of ' +questions.length;
-console.log(resultsContainer);        
+       
 
     }
 
